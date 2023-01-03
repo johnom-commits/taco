07b219afbecb23df4ac882bfddc6fa298ca277c8
Ext-E.V.Omelchenko <EOmelchenko@it-one.ru>
2023-01-03 18:16:13 +0300

    Fix error

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -5,8 +5,8 @@ import lombok.Data;
 
 @Data
 public class Taco {
-	
-	private String name;
-	private List<Ingredient> ingredients;
+
+    private String name;
+    private List<Ingredient> ingredients;
 
 }
